16821be228a4e3845aea30705620422e8d500e54
rossorozco <cdroc720@gmail.com>
2022-10-26 16:22:08 -0600

    Ingreso de datos

--- LaboratorioFinal.cpp
+++ LaboratorioFinal.cpp
@@ -19,15 +19,37 @@
 using namespace std; //Permite tener un alcance directo a todas las funciones declaradas en un espacio de nombres "std"
 
 int main(){
-	
+	char dpi[15], nombre[30], apellido[30], salario[30],resp[1];
+	int codigo=0;
 	ofstream  escritura; //Paso 2
 	
 	//Decirle que agrege informacion al final (que no lo borre) agregamos ios::app (y ios::app sirve para poder escribir en el archivo y que no borre lo que teniamos(lo añade al final)
-	escritura.open("datos.txt",ios::out|ios::app); //paso 3 (ios::out es para poder escribir en el archivo(escritura) pero borra lo que teniamos)
-	//Paso 5 (Validacion de apertura del archivo)
+	escritura.open("empleados.txt",ios::out|ios::app); //paso 3 (ios::out es para poder escribir en el archivo(escritura) pero borra lo que teniamos)
+	//Paso 4 (Validacion de apertura del archivo)
 	if(escritura.is_open()){
-		cout<<"Archivo abierto"<<endl;
-		escritura<<"Hola, hola, buenos dias segundo"<<endl;//paso 5
+		do{
+			fflush(stdin);
+			cout<<"Archivo abierto"<<endl;
+			//Ingreso de datos
+			cout<<"\n Empleado No."<<endl;
+			cout<<"\n Ingrese su Dpi: ";
+			cin>>dpi;
+			cout<<"\n Ingrese su Codigo de empleado: ";
+			cin>>codigo;
+			cout<<"\n Ingrese su Nombre: ";
+			cin>>nombre;
+			cout<<"\n Ingrese su Apellido: ";
+			cin>>apellido;
+			cout<<"\n Ingrese su salario: ";
+			cin>>salario;
+			
+			//Escribiendo datos en el archivo
+			escritura<<dpi<<" "<<codigo<<" "<<nombre<<" "<<apellido<<" "<<salario<<endl;
+			cout<<"\n Deseas ingresar datos de otro empleado (S/N))?: ";
+			cin>>resp[0];
+			cout<<"Registro Ageragado"<<endl;
+		}while(resp[0]=='S'|resp[0]=='s');//condicion de do while
+		//fin de do while
 	}
 	else{
 		cout<<"Archivo no abierto"<<endl;
